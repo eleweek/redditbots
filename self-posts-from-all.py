2927c8fccd04e124032c44466c8cb3c41bb92370
Alexander Putilin <avp-13@yandex.ru>
2015-07-12 00:39:55 +0300

    argparse for self-posts-from-all

--- self-posts-from-all.py
+++ self-posts-from-all.py
@@ -1,12 +1,18 @@
 import praw
+import argparse
+
+parser = argparse.ArgumentParser(description='Plot karma distributions of comments in a post')
+parser.add_argument('--subreddit', required=True, help='Subreddit name')
+parser.add_argument('--submission-limit', type=int, required=True, help='Limit to submission amount')
+args = parser.parse_args()
+
 
 r = praw.Reddit(user_agent='self posts from a sub by /u/godlikesme')
-submissions = r.get_subreddit('all').get_hot(limit=1000)
+submissions = [s for s in r.get_subreddit(args.subreddit).get_hot(limit=1000) if s.selftext][:args.submission_limit]
 for submission in submissions:
-    if submission.selftext:
-        print "Link:", submission.permalink
-        print "Title:", submission.title
-        print "Score:", submission.score
-        print
-        print submission.selftext
-        print "======================================"
+    print "Link:", submission.permalink
+    print "Title:", submission.title
+    print "Score:", submission.score
+    print
+    print submission.selftext
+    print "======================================"
